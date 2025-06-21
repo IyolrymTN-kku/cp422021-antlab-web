1fe1c00da92b3145973c664e3cd4bb83993ea4c9
{IyolrymTN-kku} <{nansaphat.t@kkumail.com}>
2025-06-22 02:22:48 +0700

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { createApp } from 'vue'
 import App from '@/App.vue'
 import { registerPlugins } from '@core/utils/plugins'
+import { createApp } from 'vue'
 
 // Styles
 import '@core/scss/template/index.scss'
 import '@layouts/styles/index.scss'
 
 // Create vue app
+
+
 const app = createApp(App)
 
 
